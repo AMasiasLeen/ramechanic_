5958c5f1fba42b167dbf858a45d89741e4a21d02
Jonathan Moreno <jmariomorenor@gmail.com>
2024-09-04 16:12:38 -0500

    Agregado Función para el Sidebar

--- resources/js/aside_menu.js
+++ resources/js/aside_menu.js
@@ -1,5 +1,29 @@
+
 const hamBurger = document.querySelector(".toggle-btn");
+
+const status = statusSidebar();
 
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
+  localStorage.setItem("sidebarStatus", !status);
 });
+
+function statusSidebar() {
+  const sidebarStatus = localStorage.getItem("sidebarStatus");
+
+  if (sidebarStatus == null) {
+    localStorage.setItem("sidebarStatus", false);
+    return false;
+  }
+
+  const isExpanded = sidebarStatus === "true";
+
+  if (isExpanded) {
+    document.querySelector("#sidebar").classList.add("expand");
+  } else {
+    document.querySelector("#sidebar").classList.remove("expand");
+  }
+
+
+  return isExpanded;
+}
