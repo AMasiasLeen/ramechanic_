5ca197131610c2dc2ba476c8f1a9b19ea45764f1
AMasiasLeen <leenmasias@gmail.com>
2024-09-05 16:25:29 -0500

    Update.
    - Se añaden rutas de roles y usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\VehicleModelController;
 use App\Http\Middleware\AdminMiddleware;
@@ -16,7 +18,8 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(["auth", AdminMiddleware::class])->group(function () {
-    
+    Route::resource("users", UserController::class);
+    Route::resource("roles", RoleController::class);
     Route::resource("brands", BrandController::class);
     Route::resource("vehicle-models", VehicleModelController::class);
     Route::resource("vehicles", VehicleController::class);
