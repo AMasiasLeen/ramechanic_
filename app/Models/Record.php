d6d2d37ad8aa196871ad1084e4c251727e362bf6
AMasiasLeen <leenmasias@gmail.com>
2024-09-06 17:58:21 -0500

    Update.
    - Se realizan las relaciones para los registros

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Record extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $with = ["create_uid"];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            if (Auth::check()) {
+                $model->user_id = Auth::id();
+            }
+        });
+    }
+
+    function create_uid()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    
+    function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
