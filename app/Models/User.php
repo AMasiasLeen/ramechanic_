c03c436dbe5066bc849b1d0b5e274ffd5f9a9f43
Jonathan Moreno <jmariomorenor@gmail.com>
2024-09-06 14:34:11 -0500

    Agregado Cmabio en Modelo para que se guarde el rol

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRoles;
 
-    protected $guarded = [];
+    protected $guarded = ["rol"];
 
     protected $with = ["create_uid"];
 
