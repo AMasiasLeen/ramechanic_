eae80d9d5e953875d1fe08f1acf59220ba21bd89
Jonathan Moreno <jmariomorenor@gmail.com>
2024-09-12 12:44:28 -0500

    Agregado Factory de Vehículos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Vehicle;
+use Database\Factories\VehiclesFactory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UsersTableSeeder::class);
+
+
+        Vehicle::factory(VehiclesFactory::class)->make(1000);
     }
 }
