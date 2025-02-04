21dfd5c7c7161d4b42d3f81dbaca45f9f88f8252
leenmasias <leenmasias@gmail.com>
2025-02-04 01:07:47 -0500

    cambio campo L_P

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -153,7 +153,8 @@
                                 <p><strong>Vehículo:</strong>
                                     {{ $record->vehicle->vehicle_model->brand->name }} - {{ $record->vehicle->vehicle_model->name }}</p>
                                 <p><strong>Fecha de Registro:</strong> {{ $record->date_in }}</p>
-                                <p><strong>Descripción:</strong> {{ $record->long_description }}</p>
+                                <p><strong>Descripción:</strong> {!! strip_tags($record->long_description, 
+                                    '<p><strong><em><u><ol><ul><li><table><tr><td><th><img><br><h1><h2><h3><h4><h5><h6>') !!}
                                 @if ($record->images)
                                     <div id="carousel{{ $record->id }}" class="carousel slide" data-bs-ride="carousel">
                                         <div class="carousel-inner">
