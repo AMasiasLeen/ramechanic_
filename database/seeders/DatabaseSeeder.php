9abc284fb153493dcd4f34d029adce3ef2b694b0
leenmasias <leenmasias@gmail.com>
2025-02-04 16:59:55 -0500

    ct

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(VehicleModelsTableSeeder::class);
 
 
-        Vehicle::factory()->count(20)->create();
+        Vehicle::factory()->count(60)->create();
         Record::factory()->count(25)->create();
     }
 }
